7cfafae9ae85ee5d5f4a996d622ab9da553a1a13
Sergio Segura Blanco <ssegurab@gmail.com>
2022-05-11 10:01:36 +0200

    uploading finished WeatherApp

---
+++ src/components/Card.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import Spinner from './Spinner';
+
+const Card = ({weather, forecast, loadingData, showData}) =>{
+
+    let today = new Date();
+    let hour = today.toLocaleTimeString();
+    let day = today.getDate();
+    let month = today.getMonth() +1;
+    let year = today.getFullYear();
+    let date = day + '/' + month + '/' + year;
+
+    let url = "";
+    let iconUrl = "";
+
+    let iconUrl3h = "";
+    let iconUrl6h = "";
+    let iconUrl9h = "";
+
+    let forecastDate3h = "";
+    let forecastDate6h = "";
+    let forecastDate9h = "";
+    
+    if(loadingData){
+        return <Spinner />
+    }
+
+    if(showData){
+        url = "http://openweathermap.org/img/w/";
+        iconUrl = url + weather.weather[0].icon + ".png";
+
+        iconUrl3h = url + forecast.list[1].weather[0].icon + ".png";
+        iconUrl6h = url + forecast.list[2].weather[0].icon + ".png";
+        iconUrl9h = url + forecast.list[3].weather[0].icon + ".png";
+
+        forecastDate3h = forecast.list[1].dt_txt.substring(8, 10) + '/' + forecast.list[1].dt_txt.substring(5, 7) + '/' + forecast.list[1].dt_txt.substring(0, 4) + ' ' + forecast.list[1].dt_txt.substring(11, 16);
+        forecastDate6h = forecast.list[2].dt_txt.substring(8, 10) + '/' + forecast.list[2].dt_txt.substring(5, 7) + '/' + forecast.list[2].dt_txt.substring(0, 4) + ' ' + forecast.list[2].dt_txt.substring(11, 16);
+        forecastDate9h = forecast.list[3].dt_txt.substring(8, 10) + '/' + forecast.list[3].dt_txt.substring(5, 7) + '/' + forecast.list[3].dt_txt.substring(0, 4) + ' ' + forecast.list[3].dt_txt.substring(11, 16);
+    }
+
+    return(
+        <div className="mt-5">
+
+            {
+                showData === true ? (
+                    <div className="container">
+                        <div className="card mb-3 mx-auto bg-dark text-light">
+                            <div className="row g-0">
+                                <div className="card-weather col-md-4">
+                                    <h3 className="card-title">{weather.name}</h3>
+                                    <p className="card-date">{date}</p>
+                                    <p className="card-hour">{hour.substring(0, 4) + 'h'}</p>
+                                    <h1 className="card-temp">{(weather.main.temp - 273.15).toFixed(1)}ºC</h1>
+                                    <p className="card-desc"><img src={iconUrl} alt="icon"/>{weather.weather[0].description}</p>
+                                    <img src="https://cdn.pixabay.com/photo/2019/12/24/12/31/berlin-4716680_960_720.jpg" className="card-img img-fluid rounded-start" alt="city"></img>
+                                </div>
+                                <div className="col-md-8">
+                                    <div className="card-body text-start mt-2">
+                                        <h5 className="card-text">Temperatura máxima: {(weather.main.temp_max - 273.15).toFixed(1)}ºC</h5>
+                                        <h5 className="card-text">Temperatura mínima: {(weather.main.temp_min - 273.15).toFixed(1)}ºC</h5>
+                                        <h5 className="card-text">Sensación térmica: {(weather.main.feels_like - 273.15).toFixed(1)}ºC</h5>
+                                        <h5 className="card-text">Humedad: {(weather.main.humidity)}%</h5>
+                                        <h5 className="card-text">Velocidad del viento: {(weather.wind.speed)}m/s</h5>
+                                    </div>
+                                    <hr />
+
+                                    <div className="row mt-4">
+                                        <div className="col">
+                                            <p>{forecastDate3h}h</p>
+                                            <p className="description"><img src={iconUrl3h} alt="icon"/>{forecast.list[1].weather[0].description}</p>
+                                            <p className="temp">{(forecast.list[1].main.temp - 273.15).toFixed(1)}ºC</p>
+                                        </div>
+                                        <div className="col">
+                                            <p>{forecastDate6h}h</p>
+                                            <p className="description"><img src={iconUrl6h} alt="icon"/>{forecast.list[2].weather[0].description}</p>
+                                            <p className="temp">{(forecast.list[2].main.temp - 273.15).toFixed(1)}ºC</p>
+                                        </div>
+                                        <div className="col">
+                                            <p>{forecastDate9h}h</p>
+                                            <p className="description"><img src={iconUrl9h} alt="icon"/>{forecast.list[3].weather[0].description}</p>
+                                            <p className="temp">{(forecast.list[3].main.temp - 273.15).toFixed(1)}ºC</p>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ):(
+                    <h2 className="text-light">Sin datos</h2>
+                )
+            }
+
+        </div>
+    );
+}
+
+export default Card;
---
+++ src/components/WeatherPanel.jsx
@@ -0,0 +1,78 @@
+import React, {useState} from "react";
+import Form from "./Form";
+import Card from "./Card";
+
+const WeatherPanel = () => {
+
+    let urlWeather = "https://api.openweathermap.org/data/2.5/weather?&lang=es";
+    let appId = "&appid="
+    let apiKey = process.env.REACT_APP_API_KEY;
+    let cityUrl = "&q=";
+
+    let urlForecast = "https://api.openweathermap.org/data/2.5/forecast?&lang=es";
+
+    const [weather, setWeather] = useState ([]);
+    const [forecast, setForecast] = useState ([]);
+
+    const [loading, setLoading] = useState(false);
+    const [show, setShow] = useState(false);
+    const [location, setLocation] = useState("");
+
+    const getLocation = async(loc) => {
+        setLoading(true);
+        setLocation(loc);
+
+        //Weather
+        urlWeather = urlWeather + appId + apiKey + cityUrl + loc;
+
+        await fetch(urlWeather).then((response) =>{
+            if(!response.ok) throw {response}
+            return response.json();
+        }).then((weatherData) =>{
+            console.log(weatherData);
+            setWeather(weatherData);
+        }).catch(error =>{
+            console.log(error);
+            setLoading(false);
+            setShow(false);
+        });
+
+        //Forecast
+        urlForecast = urlForecast + appId + apiKey + cityUrl + loc;
+
+        await fetch(urlForecast).then((response) =>{
+            if(!response.ok) throw {response}
+            return response.json();
+        }).then((forecastData) =>{
+            console.log(forecastData);
+            setForecast(forecastData);
+
+            setLoading(false);
+            setShow(true);
+
+        }).catch(error =>{
+            console.log(error);
+            setLoading(false);
+            setShow(false);
+        });
+
+    }
+
+    return(
+        <React.Fragment>
+            <Form 
+                newLocation = {getLocation}
+            />
+
+            <Card 
+                weather = {weather}
+                forecast = {forecast}            
+                loadingData = {loading}
+                showData = {show}
+            />
+            
+        </React.Fragment>
+    );
+}
+
+export default WeatherPanel;
